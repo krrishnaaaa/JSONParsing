bb6a9be6f7d8f71c6e00c862eb4c812aca486655
krrishnaaaa <krishna@pcsalt.com>
2015-01-01 04:02:22 +0530

    displaying list of post in custom listview

--- app/src/main/java/pcsalt/example/jsonparsing/MainActivity.java
+++ app/src/main/java/pcsalt/example/jsonparsing/MainActivity.java
@@ -1,9 +1,14 @@
 package pcsalt.example.jsonparsing;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,7 +19,7 @@ import java.util.ArrayList;
 public class MainActivity extends ActionBarActivity {
     String TAG = getClass().getSimpleName();
     JSONObject mJsonObject;
-    TextView tv;
+    ListView lvPcsPost;
     PostValue postValue;
     ArrayList<PostValue> postList = new ArrayList<PostValue>();
 
@@ -23,8 +28,16 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) findViewById(R.id.tv);
-
+        lvPcsPost = (ListView) findViewById(R.id.lvPcsPost);
+        lvPcsPost.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (postList != null && postList.size() > 0) {
+                    Intent intentShowPost = new Intent(Intent.ACTION_VIEW, Uri.parse(postList.get(position).getGuid()));
+                    startActivity(intentShowPost);
+                }
+            }
+        });
         new JSONAsync().execute();
     }
 
@@ -57,6 +70,12 @@ public class MainActivity extends ActionBarActivity {
     }
 
     class JSONAsync extends AsyncTask<Void, Void, Void> {
+        ProgressDialog pd;
+
+        @Override
+        protected void onPreExecute() {
+            pd = ProgressDialog.show(MainActivity.this, "PCSalt", "Loading posts for PCSalt.com ...", true, false);
+        }
 
         @Override
         protected Void doInBackground(Void... params) {
@@ -66,12 +85,9 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         protected void onPostExecute(Void result) {
-            tv.setText("");
-            for (PostValue value : postList) {
-                tv.append("\n\nTitle: " + value.getTitle());
-                tv.append("\nGuid: " + value.getGuid());
-                tv.append("\nDate: " + value.getDate());
-            }
+            PostBaseAdapter postBaseAdapter = new PostBaseAdapter(MainActivity.this, postList);
+            lvPcsPost.setAdapter(postBaseAdapter);
+            pd.dismiss();
         }
     }
 
